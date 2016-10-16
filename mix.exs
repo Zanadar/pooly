7a0cb0c229ce79a3fbe3adcf4160273af46447c3
Zander Mackie <zmackie@gmail.com>
2016-10-16 14:49:52 -0400

    pg136

--- mix.exs
+++ mix.exs
@@ -14,18 +14,10 @@ defmodule Pooly.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: [:logger],
+     mod: {Pooly, []}]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     []
   end
